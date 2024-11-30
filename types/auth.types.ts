36e9efe7be7fb6325d4d43f4e50c46702eee1192
Zahra <Zahrabazrgari@outlook.com>
2024-12-01 03:08:33 +0330

    api fixes

---
+++ types/auth.types.ts
@@ -0,0 +1,46 @@
+export interface IToken {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export enum IRole {
+  "USER",
+  "ADMIN",
+}
+
+export interface IUser {
+  _id: string;
+  firstname: string;
+  lastname: string;
+  username: string;
+  password: string;
+  phoneNumber: string;
+  address: string;
+  role: IRole;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+  refreshToken: string;
+}
+
+export interface ILoginRequest {
+  username: string;
+  password: string;
+}
+
+export interface ISignUpRequest {
+  firstname: string;
+  lastname: string;
+  username: string;
+  password: string;
+  phoneNumber: string;
+  address: string;
+}
+
+export interface IAuthResponse {
+  status: string;
+  token: IToken;
+  data: {
+    user: IUser;
+  };
+}
